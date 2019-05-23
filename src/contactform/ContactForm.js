af7ad18300e9aaefdc628d26b376a2af696b5c70
Fabio Romanini <fabioaromanini@gmail.com>
2019-05-23 12:05:55 -0300

    contact-form - adds contact-forms' header and body

--- src/contactform/ContactForm.js
+++ src/contactform/ContactForm.js
@@ -3,7 +3,14 @@ import React from 'react';
 import './ContactForm.css';
 
 function ContactForm() {
-  return <div className="ContactForm">Formulário de contato</div>;
+  return (
+    <div className="ContactForm">
+      <div className="ContactForm-header">
+        <h1>Formulário de contato</h1>
+      </div>
+      <div className="ContactForm-body">Body</div>
+    </div>
+  );
 }
 
 export default ContactForm;
