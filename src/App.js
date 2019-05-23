231a362ad6d2e43ef33cc1f73871c6a938f40911
Fabio Romanini <fabioaromanini@gmail.com>
2019-05-23 10:49:41 -0300

    backbone - adds contact form section

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import Landing from './landing/Landing';
+import ContactForm from './contactform/ContactForm';
 
 import './App.css';
 
@@ -8,6 +9,7 @@ function App() {
   return (
     <div className="App">
       <Landing />
+      <ContactForm />
     </div>
   );
 }
---
+++ src/contactform/ContactForm.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import './ContactForm.css';
+
+function ContactForm() {
+  return <div className="ContactForm">Formulário de contato</div>;
+}
+
+export default ContactForm;
