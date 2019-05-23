96d04e58a28105be9e1fc99a35f52b1b5b61f47a
Fabio Romanini <fabioaromanini@gmail.com>
2019-05-23 13:13:36 -0300

    contact-form - adds inputs

--- src/contactform/ContactForm.js
+++ src/contactform/ContactForm.js
@@ -2,15 +2,76 @@ import React from 'react';
 
 import './ContactForm.css';
 
-function ContactForm() {
-  return (
-    <div className="ContactForm">
-      <div className="ContactForm-header">
-        <h1>Formulário de contato</h1>
+class ContactForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      name: '',
+      message: ''
+    };
+  }
+
+  handleEmailChange = event => {
+    this.setState({ email: event.target.value });
+  };
+
+  handleNameChange = event => {
+    this.setState({ name: event.target.value });
+  };
+
+  handleMessageChange = event => {
+    this.setState({ message: event.target.value });
+  };
+
+  handleSubmit = event => {
+    console.log(this.state);
+    event.preventDefault();
+  };
+
+  render() {
+    return (
+      <div className="ContactForm">
+        <div className="ContactForm-header">
+          <h1>Formulário de contato</h1>
+        </div>
+        <form onSubmit={this.handleSubmit}>
+          <div className="ContactForm-body">
+            <label>
+              Email:
+              <input
+                type="text"
+                value={this.state.email}
+                onChange={this.handleEmailChange}
+              />
+            </label>
+            <label>
+              Name:
+              <input
+                type="text"
+                value={this.state.name}
+                onChange={this.handleNameChange}
+              />
+            </label>
+
+            <label>
+              Message:
+              <input
+                type="text"
+                value={this.state.message}
+                onChange={this.handleMessageChange}
+              />
+            </label>
+            <input
+              type="submit"
+              value="Submit"
+              className="ContactForm-button"
+            />
+          </div>
+        </form>
       </div>
-      <div className="ContactForm-body">Body</div>
-    </div>
-  );
+    );
+  }
 }
 
 export default ContactForm;
