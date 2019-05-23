980469ce9742380e91fcc113cfdd62fca11a256a
Fabio Romanini <fabioaromanini@gmail.com>
2019-05-23 10:44:02 -0300

    backbone - refactors landing page to a separate component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
+
+import Landing from './landing/Landing';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <p>Lorem.</p>
-      </header>
+      <Landing />
     </div>
   );
 }
---
+++ src/landing/Landing.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Landing.css';
+
+function Landing() {
+  return (
+    <header className="Landing-header">
+      {/* <img src={logo} className="Landing-logo" alt="logo" /> */}
+      <p>Lorem.</p>
+    </header>
+  );
+}
+
+export default Landing;
