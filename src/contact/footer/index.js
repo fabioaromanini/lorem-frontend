b2cba4306718f6ee73ee741f57da7f71a901c047
Fabio Romanini <fabioaromanini@gmail.com>
2019-05-26 10:36:43 -0300

    contact-form-validation - adds responsiveness to footer text

--- src/contact/footer/index.js
+++ src/contact/footer/index.js
@@ -10,7 +10,7 @@ export default () => {
     <footer className="Footer">
       <Container>
         <Row>
-          <Col className="Footer-banner" xs={{ offset: 4, span: 4 }}>
+          <Col className="Footer-banner" xs={12} md={{ span: 4, offset: 4 }}>
             Made with <p id="Footer-heart">❤</p> by{' '}
             <a
               rel="noopener noreferrer"
@@ -22,7 +22,7 @@ export default () => {
           </Col>
         </Row>
         <Row>
-          <Col xs={{ span: 2, offset: 5 }}>
+          <Col xs={{ span: 6, offset: 3 }} md={{ span: 2, offset: 5 }}>
             <div className="Footer-links">
               <p>github:</p>
               <a
