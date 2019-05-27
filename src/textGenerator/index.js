e5b7959d6b8eaf91c92eef0658ed4cb9456d85bb
Fabio Romanini <fabioaromanini@gmail.com>
2019-05-27 12:10:58 -0300

    text-generation - adds output text placeholder

--- src/textGenerator/index.js
+++ src/textGenerator/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 
+import mockText from './mocktext';
+
 import './TextGenerator.css';
 
 function TextGenerator() {
   return (
     <div id="TextGenerator" className="TextGenerator">
       <h1 className="Section-title">Generate text</h1>
+      <textarea id="TextGenerator-output" disabled value={mockText} />
     </div>
   );
 }
